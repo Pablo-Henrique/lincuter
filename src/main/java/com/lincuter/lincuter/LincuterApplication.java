c7c05dd98ba3ec05ba30c34385df6d0b78f579e3
Pablo Henrique <pabloh.anselmo@gmail.com>
2026-04-27 19:21:36 -0300

    first commit

---
+++ src/main/java/com/lincuter/lincuter/LincuterApplication.java
@@ -0,0 +1,13 @@
+package com.lincuter.lincuter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LincuterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LincuterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lincuter/lincuter/LincuterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lincuter.lincuter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LincuterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
